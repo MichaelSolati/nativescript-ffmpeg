d43850912822913b8e9316109faf69b80c7284bd
Cliff Jefferson Sumalpong <cliffjeffersonsumalpog@Cliffs-iMac.local>
2018-05-31 21:13:41 +0800

    Updated FFmpeg binary and library Supports FFprobe

--- src/ffmpeg.android.ts
+++ src/ffmpeg.android.ts
@@ -6,9 +6,10 @@ import * as utils from 'tns-core-modules/utils/utils';
 import { Callback, Common } from './ffmpeg.common';
 
 declare const android: any;
-declare const com: any;
-const bundle = com.github.hiteshsondhi88.libffmpeg;
+declare const nl, com: any;
+const bundle = nl.bravobit.ffmpeg;
 const ffmpeg = bundle.FFmpeg.getInstance(app.android.context);
+const ffprobe = bundle.FFprobe.getInstance(app.android.context);
 
 export class FFmpeg extends Common {
   static execute(command: string | Array<string>, callback?: Callback, debug: boolean = false): void {
@@ -22,12 +23,9 @@ export class FFmpeg extends Common {
         } else if (Array.isArray(command) && command[0] === 'ffmpeg') {
           command.shift();
         }
-        try {
-          ffmpeg.loadBinary(new bundle.LoadBinaryResponseHandler());
-        } catch (e) {
-          if (callback) { callback('FFmpeg is not supported by device'); }
-          return;
-        }
+
+        if (!ffmpeg.isSupported()) return;
+
         const ExecuteBinaryResponseHandler = bundle.ExecuteBinaryResponseHandler.extend({
           onStart: () => { if (debug) { console.log('Running FFmpeg'); } },
           onProgress: (message) => { if (debug) { console.log(message); } },
@@ -48,6 +46,49 @@ export class FFmpeg extends Common {
         });
         try {
           ffmpeg.execute(command, new ExecuteBinaryResponseHandler());
+        } catch (e) {
+          if (callback) { callback(`Could not execute: ${command.join(' ')}`); }
+          return;
+        }
+      }).catch(() => { if (callback) { callback('Permissions denied'); } });
+  }
+}
+
+export class FFprobe extends Common {
+  static execute(command: string | Array<string>, callback?: Callback, debug: boolean = false): void {
+    Permissions.requestPermission(
+      [android.Manifest.permission.READ_EXTERNAL_STORAGE,
+      android.Manifest.permission.WRITE_EXTERNAL_STORAGE],
+      'We need access to the file system to save your file').then(() => {
+        if (typeof command === 'string') {
+          command.replace('ffprobe ', '');
+          command = command.split(' ');
+        } else if (Array.isArray(command) && command[0] === 'ffprobe') {
+          command.shift();
+        }
+
+        if (!ffprobe.isSupported()) return;
+
+        const ExecuteBinaryResponseHandler = bundle.ExecuteBinaryResponseHandler.extend({
+          onStart: () => { if (debug) { console.log('Running FFprobe'); } },
+          onProgress: (message) => { if (debug) { console.log(message); } },
+          onFailure: (message) => {
+            if (debug) { console.log(message); }
+            if (callback) { callback(message); }
+            return;
+          },
+          onSuccess: (message) => {
+            if (debug) { console.log('Successfully run FFprobe'); }
+            if (callback) { callback(); }
+            return;
+          },
+          onFinish: () => {
+            if (callback) { callback(); }
+            return;
+          }
+        });
+        try {
+          ffprobe.execute(command, new ExecuteBinaryResponseHandler());
         } catch (e) {
           if (callback) { callback(`Could not execute: ${command.join(' ')}`); }
           return;
--- src/ffmpeg.ios.ts
+++ src/ffmpeg.ios.ts
@@ -11,3 +11,15 @@ export class FFmpeg extends Common {
     if (callback) { callback('iOS is not supported at this time'); }
   }
 }
+
+export class FFprobe extends Common {
+  static execute(command: string | Array<string>, callback?: Callback, debug: boolean = false): void {
+    if (typeof command === 'string') {
+      command.replace('ffprobe ', '');
+      command = command.split(' ');
+    } else if (Array.isArray(command) && command[0] === 'ffprobe') {
+      command.shift();
+    }
+    if (callback) { callback('iOS is not supported at this time'); }
+  }
+}
